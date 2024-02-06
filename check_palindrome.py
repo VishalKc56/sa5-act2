fb7acd64b31dc25e5e983207ad048c6fc6ac17d5
Katie Bridson <kbridson@memphis.edu>
2024-02-05 18:43:18 -0600

    Replaced hardcoded text string with filepath input and read file content
    Fixed bugs with filepath variable, added more specific exception
    
    Added choice of text input or filepath input and read file content

--- check_palindrome.py
+++ check_palindrome.py
@@ -5,5 +5,9 @@ def check_palindrome(text):
         print(f"{text} is not a palindrome")
 
 
-text = input("Enter text to check for palindrome: ")
-check_palindrome(text)
+try:
+    filepath = input("Enter filepath for palindrome check: ")
+    text = open(filepath, "r").read()
+    check_palindrome(text)
+except OSError:
+    print(f"Unable to process file at {filepath}")
